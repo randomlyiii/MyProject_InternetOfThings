3be50de8242c17d330583cffa58fe3cb5ee1e510
randomlyiii <2313346840@qq.com>
2026-03-12 18:08:21 +0800

    Basically_Completed

---
+++ Linux_Deploy/mysql/init/mydate.sql
@@ -0,0 +1,36 @@
+--
+-- Current Database: `back`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `back` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `back`;
+
+--
+-- Table structure for table `clazz`
+--
+
+DROP TABLE IF EXISTS `mydate`;
+-- 操作日志表
+create table mydata(
+                    id int unsigned primary key auto_increment comment 'ID',
+                    temperature float comment '温度',
+                    humidity float comment '湿度',
+                    write_time datetime comment '记录时间'
+) comment '数据表';
+
+insert into mydata(temperature, humidity, write_time) values (null, null, null);
+
+UNLOCK TABLES;
+
+-- 1. 先删除所有旧的 app_user 用户，避免权限冲突（必须执行）
+DROP USER IF EXISTS 'app_user'@'%';
+
+-- 2. 创建用户：允许 任意IP(%) 登录，密码 Test@123
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'Test@123';
+
+-- 3. 核心权限：仅授予 back 库的所有权限，无法操作其他库
+GRANT ALL PRIVILEGES ON back.* TO 'app_user'@'%';
+
+-- 4. 刷新权限，立即生效
+FLUSH PRIVILEGES;
---
+++ Project_STM32/README.md
@@ -0,0 +1,18 @@
+# OMIT VCC、GND
+# STM32F103C8T6:
+
+# OLED:
+    SCL -> PB8
+    SDA -> PB9
+
+# DHT11:
+    I/O -> PB11
+    EN  -> default 悬空
+
+# ESP8266(esp-01s):
+    RX  -> PA9
+    TX  -> PA10
+    EN  -> 3.3V
+    RST -> 3.3V
+    GPIO0 -> 3.3V
+    GPIO2 -> 3.3V
